acfd2f35f63e3636318276881b6d02fd835b3f07
王鹏 <wangpeng_f@aspirecn.com>
2021-01-25 22:16:15 +0800

    init

---
+++ server.js
@@ -0,0 +1,17 @@
+let http = require('http')
+let users = [
+  {id: 1, name: 'zhufeng1'},
+  {id: 2, name: 'zhufeng2'},
+  {id: 3, name: 'zhufeng3'},
+]
+let server = http.createServer(function(req, res) {
+  if (req.url === '/api/users') {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.end(JSON.stringify(users))
+  } else {
+    res.end('not found')
+  }
+})
+server.listen(3001, () => {
+  console.log('已经启动在3001端口上')
+})
