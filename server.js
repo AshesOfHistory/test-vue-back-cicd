aa3afa42452b5fdb840316e40c5152d2c049722c
王鹏 <wangpeng_f@aspirecn.com>
2021-01-30 00:47:39 +0800

    122

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let http = require('http')
 let users = [
-  {id: 122, name: 'zhufeng1'},
-  {id: 223, name: 'zhufeng2'},
-  {id: 321, name: 'zhufeng3'},
+  {id: 111, name: 'zhufeng1'},
+  {id: 222, name: 'zhufeng2'},
+  {id: 333, name: 'zhufeng3'},
 ]
 let server = http.createServer(function(req, res) {
   if (req.url === '/api/users') {
