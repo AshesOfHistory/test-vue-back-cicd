509d85b1bac6cabe2dfaf51a85658efa1d05c3d6
王鹏 <wangpeng_f@aspirecn.com>
2021-01-30 14:21:48 +0800

    111

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 let http = require('http')
 let users = [
-  {id: 1121222222, name: 'zhufeng1'},
+  {id: 122, name: 'zhufeng1'},
   {id: 2, name: 'zhufeng2'},
   {id: 3, name: 'zhufeng3'},
   {id: 4, name: 'zhufeng4'},
